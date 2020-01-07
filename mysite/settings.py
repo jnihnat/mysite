02c2a2fbff618254408c46699d135e8063c08e2a
jnihnat <jnihnat@gmail.com>
2020-01-07 13:28:27 +0100

    Initial commit
    Signed-off-by: jnihnat <jnihnat@gmail.com>

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,12 +78,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR2, 'db.sqlite3'),
-        'CONN_MAX_AGE': 5,
-        'OPTIONS': {
-            'timeout': 60,
-        }
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR2, 'db.sqlite3'),
+#        'CONN_MAX_AGE': 5,
+#        'OPTIONS': {
+#            'timeout': 60,
+#       }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysitedb',
+        'USER': 'mysiteuser',
+        'PASSWORD': 'password',
+        'HOST': '13.80.146.15',
+        'PORT': '3306',
     },
 
 }
