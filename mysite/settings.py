3a25913150366cd1d58310e54b19ea81b2c498b0
jnihnat <jnihnat@gmail.com>
2020-01-13 22:04:51 +0100

    Initial commit
    Signed-off-by: jnihnat <jnihnat@gmail.com>

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/app/static/'
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'venv/Lib/site-packages/django/contrib/admin/static/'),
  )
